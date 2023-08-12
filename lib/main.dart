67bb2300cea1a2a3aebfa8c603540671646182da
Muhesi <muhesimuyisa@gmail.com>
2023-08-12 22:36:41 +0200

    first veresion main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,6 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold();
   }
 }
