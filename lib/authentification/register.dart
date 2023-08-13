f96b9327866aeda8e5b98967b3dc873801a2a443
Muhesi <muhesimuyisa@gmail.com>
2023-08-13 19:16:10 +0200

    changed

--- lib/authentification/register.dart
+++ lib/authentification/register.dart
@@ -1,4 +1,5 @@
 import 'package:apisaissai/colors/color.dart';
+import 'package:apisaissai/widgets/fieldText.dart';
 import 'package:flutter/material.dart';
 
 class Register extends StatefulWidget {
@@ -9,13 +10,35 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+
+  // Appel des instance de la classe champs 
+  CustomFieldeText txtNom= CustomFieldeText(
+    title: 'Nom',
+    placeholder: 'Entrez votre Nom',
+  );
+  CustomFieldeText txtPostnom= CustomFieldeText(
+    title: "Postnom",
+    placeholder: "Entrez votre Postnom"
+  );
+  CustomFieldeText txtEmail = CustomFieldeText(
+    title: 'Email',
+    placeholder: 'Entrez votre adresse mail'
+  );
+  CustomFieldeText txtAdresse = CustomFieldeText(
+    title: 'Adresse',
+    placeholder: 'Entrez votre Adresse'
+  );
+  CustomFieldeText txtPhone = CustomFieldeText(
+    title: "Téléphone",
+    placeholder: 'Entrez votre numéro de téléphone'
+  );
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: SingleChildScrollView(
             child: Container(
-              padding: EdgeInsets.all(30),
+              padding: const EdgeInsets.all(30),
 
               // Le formulaire d'enregistrement
           child: Form(
@@ -27,8 +50,17 @@ class _RegisterState extends State<Register> {
                   textAlign: TextAlign.center,
                   style: TextStyle(color: Colors.red,fontSize: 20,fontWeight: FontWeight.bold),
                 ),
-          
-          
+                const SizedBox(height: 10,),
+                txtNom.textFormField(),
+                const SizedBox(height: 10,),
+                txtPostnom.textFormField(),
+                const SizedBox(height: 10,),
+                txtAdresse.textFormField(),
+                const SizedBox(height: 10,),
+                txtPhone.textFormField(),
+                const SizedBox(height: 10,),
+                txtPhone.textFormField(),
+                const SizedBox(height: 10,),
                 // Button 
                  ElevatedButton(
                       child: const Text("Enregistrer"),
