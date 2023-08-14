8031c02da7ceb2d6665a45fa091307a68dd07feb
Muhesi <muhesimuyisa@gmail.com>
2023-08-14 19:54:28 +0200

    channel_model.dart

--- lib/models/channel_model.dart
+++ lib/models/channel_model.dart
@@ -1,9 +1,24 @@
-class Channel{
-  final String id;
-  final String title;
-  final String profilPictureUrl;
-  final String souscriberCount;
-  final String videoCount;
-  final String iuploadPlayList;
-  List<Video>
-}
+// class Channel{
+//   final String id;
+//   final String title;
+//   final String profilPictureUrl;
+//   final String souscriberCount;
+//   final String videoCount;
+//   final String iuploadPlayList;
+//   final List<Video> videos;
+//   Channel({
+//     this.id="",
+//     this.title="",
+//     this.profilPictureUrl="",
+//     this.souscriberCount="",
+//     this.videoCount="",
+//     this.iuploadPlayList=""
+//     this.videos
+//   });
+
+//   factory Channel.fromJson(Map<String, dynamic> map){
+//     return Channel(
+//       id: map['id'],
+//     );
+//   }
+// }
