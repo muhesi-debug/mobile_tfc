c879d4e21957c9660477ab9cb2231fd2cf49320d
Muhesi <muhesimuyisa@gmail.com>
2023-08-15 14:15:39 +0200

    Changed

--- lib/models/channel_model.dart
+++ lib/models/channel_model.dart
@@ -1,24 +1,30 @@
-// class Channel{
-//   final String id;
-//   final String title;
-//   final String profilPictureUrl;
-//   final String souscriberCount;
-//   final String videoCount;
-//   final String iuploadPlayList;
-//   final List<Video> videos;
-//   Channel({
-//     this.id="",
-//     this.title="",
-//     this.profilPictureUrl="",
-//     this.souscriberCount="",
-//     this.videoCount="",
-//     this.iuploadPlayList=""
-//     this.videos
-//   });
+import 'package:apisaissai/models/video_model.dart';
 
-//   factory Channel.fromJson(Map<String, dynamic> map){
-//     return Channel(
-//       id: map['id'],
-//     );
-//   }
-// }
+class Channel{
+  final String id;
+  final String title;
+  final String profilPictureUrl;
+  final String souscriberCount;
+  final String videoCount;
+  final String uploadPlayListId;
+  late List<Video> videos;
+  Channel({
+    this.id="",
+    this.title="",
+    this.profilPictureUrl="",
+    this.souscriberCount="",
+    this.videoCount="",
+    this.uploadPlayListId=""
+  });
+
+  factory Channel.fromJson(Map<String, dynamic> map){
+    return Channel(
+      id: map['id'],
+      title: map['snippet']['title'],
+      profilPictureUrl: map['snippet']['thumbnails']['default']['url'],
+      souscriberCount: map['statistics']['subscriberCount'],
+      videoCount: map['statistics']['videoCount'],
+      uploadPlayListId:map['contentDetails']['relatedPlayLists']['uploads'] 
+    );
+  }
+}
---
+++ lib/models/video_model.dart
@@ -0,0 +1,20 @@
+class Video{
+  final String id;
+  final String title;
+  final String thumbnailsUrl;
+  final String channelTitle;
+  Video({
+    this.id='',
+    this.title='',
+    this.channelTitle='',
+    this.thumbnailsUrl=''
+  });
+  factory Video.fromJson(Map<String, dynamic> map){
+    return Video(
+      id: map['ressourceId']['videoId'],
+      title: map['title'],
+      thumbnailsUrl: map['thumbnails']['high']['url'],
+      channelTitle: map['channelTitle']
+    );
+  }
+}
